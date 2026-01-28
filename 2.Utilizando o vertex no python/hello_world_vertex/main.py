0510c3e737b26b9774436d888653e2231a528501
DadosComCafe <dadoscomcafe.dev@gmail.com>
2026-01-27 22:06:10 -0300

    (2/hello_world): Criando um hello world do python com vertex

--- 2.Utilizando o vertex no python/hello_world_vertex/main.py
+++ 2.Utilizando o vertex no python/hello_world_vertex/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from hello-world-vertex!")
+from google import genai
+from decouple import config
 
+PROJECT_ID = config("PROJECT_ID") 
+REGION = config("REGION") 
+MODEL_NAME = config("MODEL_NAME")
 
-if __name__ == "__main__":
-    main()
+client = genai.Client(vertexai=True, project=PROJECT_ID, location=REGION)
+response = client.models.generate_content(
+    model=MODEL_NAME,
+    contents="Você é uma inteligência artificial?",
+)
+print({"Você é uma inteligência artificial?": response.text})
